ab5ec9c19ee55b0b94c7b9a31fe5049f67677ae1
AndreaOscal <heydyoscal82@gmail.com>
2024-06-14 18:33:16 -0600

    Primer Commit

---
+++ src/main/java/Profesor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author andrea
+ */
+public class Profesor {
+    public String nombre; 
+    public String apellido; 
+    public String usuario; 
+    public String password; 
+    
+}
---
+++ src/main/java/com/mycompany/logina/LoginA.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.logina;
+
+/**
+ *
+ * @author andrea
+ */
+public class LoginA {
+
+    public static void main(String[] args) {
+       Login l=new Logil(); 
+       
+       
+    }
+}
